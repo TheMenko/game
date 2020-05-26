4206001adb1eaa1206410c7a1cee339f2bf8b2a4
brae <brae@braebie.com>
2020-05-26 00:10:07 -0700

    + unit conversions to speedo label
    Converts the incoming float value to a specific unit. Includes
    energy though less than ideal since it requires more info from the player.

--- mp/src/game/client/momentum/ui/HUD/hud_speedometer_label.h
+++ mp/src/game/client/momentum/ui/HUD/hud_speedometer_label.h
@@ -10,7 +10,7 @@ class SpeedometerLabel : public vgui::Label
     DECLARE_CLASS_SIMPLE(SpeedometerLabel, vgui::Label);
 
   public:
-    SpeedometerLabel(Panel *parent, const char *panelName);
+    SpeedometerLabel(Panel *parent, const char *panelName, bool supportsEnergyUnits = false);
     
     // parent's layout depends on the visiblity of this
     void SetVisible(bool bVisible) OVERRIDE { BaseClass::SetVisible(bVisible); GetParent()->InvalidateLayout(); }
@@ -20,9 +20,24 @@ class SpeedometerLabel : public vgui::Label
     void SetText(int value);
     void SetText(float value) { SetText(RoundFloatToInt(value)); }
 
+
+    // getter/setters
+    SpeedometerUnits_t GetUnitType() { return m_eUnitType; }
+    void SetUnitType(SpeedometerUnits_t type) { m_eUnitType = type; }
+    void SetUnitType(int type) { SetUnitType(SpeedometerUnits_t(type)); }
+
+    bool GetSupportsEnergyUnits() { return m_bSupportsEnergyUnits; }
+
   private:
+    void ConvertUnits();
 
     float m_flCurrentValue;
     float m_flPastValue;
     float m_flDiff;
+
+    bool m_bSupportsEnergyUnits;
+
+    SpeedometerUnits_t m_eUnitType;
+
+    ConVarRef m_cvarGravity;
 };
